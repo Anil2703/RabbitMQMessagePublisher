73d8249e1c2b2a958c00dc093693d5ff755949c2
Anil2703 <39698174+Anil2703@users.noreply.github.com>
2020-12-13 15:38:30 +0530

    Anil_UpdatedLogger

--- src/main/java/com/rocky/controller/MessagePublisherController.java
+++ src/main/java/com/rocky/controller/MessagePublisherController.java
@@ -3,6 +3,8 @@ package com.rocky.controller;
 
 import com.rocky.model.RabbitMQMessage;
 import com.rocky.service.MessagePublishService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,16 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MessagePublisherController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MessagePublisherController.class);
+
     @Autowired
     private MessagePublishService messagePublishService;
 
     @PostMapping("/publishMessage")
     public String publishMessageController(@RequestBody RabbitMQMessage rabbitMQMessage) {
 
+        LOG.info("Received RabbitMQ Message in MessagePublisherController with customerID", rabbitMQMessage.getCustomerID());
+
         try {
             messagePublishService.publishMessage(rabbitMQMessage);
+
+            LOG.info("Message Published to RabbitMQ Successfully");
             return "Message Published to RabbitMQ Successfully";
         } catch (Exception e) {
+            LOG.error("Error sending message to RabbitMQ");
             return "Error Publishing Message to RabbitMQ";
         }
 
